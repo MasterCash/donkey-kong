08f4940d9ad743f7b4a702bc329745da3f56315c
Cash <mastercash@github.com>
2018-10-08 18:21:14 -0500

    Set up InputManager to use the EventManager

--- inputManager.py
+++ inputManager.py
@@ -3,10 +3,11 @@
 #################
 import pygame
 from pygame.locals import *
-
+import queue
 #from enums import PlayerState
 from utils import Singleton
-
+from eventManager import Events, EventManager
+from collections import deque
 
 @Singleton
 class InputManager:
@@ -14,28 +15,35 @@ class InputManager:
     def __init__(self):
         self._keyFuncDict = {}
         self.keysPressed = []
-
-    def listenFor(self, keys, func):
+        self._keysQueued = deque()
+        EventManager.subscribe(Events.KEYDOWN, self.addKey)
+        EventManager.subscribe(Events.KEYUP, self.removeKey)
+        
+    def subscribe(self, keys, func):
         for key in keys:
             if self._keyFuncDict.get(key, False) is False:
                 self._keyFuncDict[key] = [func]
             self._keyFuncDict[key].append(func)
         print("Listen For Called", key, str(func))
     
-    def stopListening(self, keys, func):
+    def unsubscribe(self, keys, func):
         for key in keys:
             self._keyFunDic[key].remove(func)
         print("Stop Listening Called")
   
     def check(self):
-        pressed = pygame.key.get_pressed()
-        for key in self._keyFuncDict:
-            if pressed[key]:
-                for func in self._keyFuncDict[key]:
-                    func(key)
-
-    def checkWithEvents(self):
         for key in self.keysPressed:
-            if key in self.keyFuncDict:
+            self._keysQueued.append(key)
+        for key in self._keysQueued:
+            if key in self._keyFuncDict:
                 for func in self._keyFuncDict[key]:
                     func(key)
+        self._keysQueued.clear()
+
+    def addKey(self, key):
+        if key not in self.keysPressed:
+            self.keysPressed.append(key)
+
+    def removeKey(self, key):
+        if key in self.keysPressed:
+            self.keysPressed.remove(key)
