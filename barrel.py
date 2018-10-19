46c6540023d3af58046387eed7796841dac1f0f2
Cash <mastercash@github.com>
2018-10-18 19:22:44 -0500

    fixed some more issues involving merge issues

--- barrel.py
+++ barrel.py
@@ -5,26 +5,28 @@ import pygame
 from utils import Spritesheet
 from game import GameObject
 from spriteManager import SpriteManager
+from enum import Enum
 
-class BarrelType(enum):
+class BarrelType(Enum):
     NORMAL = 0
     EXPLOSIVE = 1
     OIL = 2
 
+class BarrelState(Enum):
+    MOVE = 0
+    FALL = 1
 
-<<<<<<< HEAD
 class Barrel(GameObject): 
     def __init__(self):
         GameObject.__init__(self)
         self.spriteManager = SpriteManager()
-        self._type = bType
         self._speed = 10
         self._sheet = Spritesheet('barrel')
 
         self._sprites = {
-            'normal_roll1': self._sheet.sprite_at((112,0,24,20))
-            'normal_roll2': self._sheet.sprite_at((160,0,24,20))
-            'normal_roll3': self._sheet.sprite_at((208,0,24,20))
+            'normal_roll1': self._sheet.sprite(112, 0, 24, 20),
+            'normal_roll2': self._sheet.sprite(160, 0, 24, 20),
+            'normal_roll3': self._sheet.sprite(208, 0, 24, 20),
             'fall_1': self._sheet.sprite(290, 0, 40, 20), 
             'fall_2': self._sheet.sprite(340, 0, 40, 20)
         }#I current do not have access to any image software so I can't measure anything
