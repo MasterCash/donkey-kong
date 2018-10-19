bfca623d6665bce0effac6c8a071d1d95b77ce7c
Lucas Belshoff <lgbktb@mst.edu>
2018-10-18 19:17:52 -0500

    sprites moving properly leftright

--- spriteManager.py
+++ spriteManager.py
@@ -3,33 +3,34 @@ from framework import SpriteSheet
 
 class SpriteManager:
     """ Used for animating sprites on a thingy """
-    def __init__(self):
-        return
-
-    #makes the sprites in sheet
-    def addSprites(self, sprites):
+    #makes the sprites in sheet 
+    def __init__(self, sprites):
         self._sprites = sprites
-        self._counter = 0 #counts the number of frames between each animation
+        self.currentAnimation = []
         return
-
-    #converts the (unordered) dictionary to an ordered array based on animation var
-    def useSprites(self, animation):
+    #converts the dictionary to an ordered array based on animation var
+    def useSprites(self, animation, tick):
+        if self.currentAnimation == animation:
+            return
+        self.currentAnimation = animation
         self._currentSpriteName = animation[0]
         self._location = 0
+        self._tick = tick
         self._currentSprite = self._sprites[self._currentSpriteName]
         self._spriteArray = []
+        self._counter = 0 #counts the number of frames between each animation
 
         for x in animation:
             self._spriteArray.append(self._sprites[x])
+        return
 
 
-    def animate(self, counter):
-        #Animates every 'counter' frames, use 1 to animate every frame, higher values update less often
+
+    def animate(self):
+        #Animates every 'tick' frames, use 1 to animate every frame, higher values update less often
         self._counter += 1
-        if self._counter == counter:
-
-            #loc = self._spriteArray.index(self._currentSprite)
-            #print(self._location)
+        if self._counter == self._tick:
+            
             #increments to the next frame of movement in current animation
             if self._location == len(self._spriteArray) - 1:
                 self._currentSprite = self._spriteArray[0]
@@ -37,7 +38,7 @@ class SpriteManager:
             else:
                 self._currentSprite = self._spriteArray[self._location+1]
                 self._location += 1
-
+            
             self._counter = 0
         else:
             pass
