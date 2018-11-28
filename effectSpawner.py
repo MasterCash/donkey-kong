e46d2027065c70a1253fc553ec6ed21f96bd6044
Michael Rouse <mwrouse@outlook.com>
2018-11-28 10:43:50 -0600

    Changed timing of the goo animation. Added an explosion animation.

--- effectSpawner.py
+++ effectSpawner.py
@@ -35,16 +35,22 @@ class Explosion(GameObject):
         self._barrel = barrel
         self.y = barrel.y
         self._sprites = {
-            'explosion_0': self._sheet.sprite(0,0,3,3),
-            'explosion_1': self._sheet.sprite(4,0,10,10),
-            'explosion_2': self._sheet.sprite(15,0,15,15),
-            'explosion_3': self._sheet.sprite(31,0,20,20),
-            'explosion_4': self._sheet.sprite(52,0,25,25),
-            'explosion_5': self._sheet.sprite(78,0,35,35),
-            'explosion_6': self._sheet.sprite(114,0,45,45),
-            'explosion_7': self._sheet.sprite(160,0,75,50),
-            'explosion_8': self._sheet.sprite(0,0,0,0)
+            'explosion_0': self._sheet.sprite(0, 57, 28, 24),
+            'explosion_1': self._sheet.sprite(48, 57, 28, 24),
+            'explosion_2': self._sheet.sprite(96, 57, 28, 24),
+            'explosion_3': self._sheet.sprite(142, 55, 32, 28),
+            'explosion_4': self._sheet.sprite(189, 52, 46, 38),
+            'explosion_5': self._sheet.sprite(176, 93, 69, 48)
         }
+        """'explosion_0': self._sheet.sprite(0,0,3,3),
+        'explosion_1': self._sheet.sprite(4,0,10,10),
+        'explosion_2': self._sheet.sprite(15,0,15,15),
+        'explosion_3': self._sheet.sprite(31,0,20,20),
+        'explosion_4': self._sheet.sprite(52,0,25,25),
+        'explosion_5': self._sheet.sprite(78,0,35,35),
+        'explosion_6': self._sheet.sprite(114,0,45,45),
+        'explosion_7': self._sheet.sprite(160,0,75,50),
+        'explosion_8': self._sheet.sprite(0,0,0,0)"""
 
         self.len = (len(self._sprites))
 
@@ -58,12 +64,11 @@ class Explosion(GameObject):
         self.centerX = self._barrel.centerX
         self.bottom = self._barrel.bottom
         self.y -= 3
-        self.spriteManager.animate()
         if self.getSprite() == self._sprites['explosion_'+ str(self.len - 1)]:
             self.kill()
 
     def getSprite(self):
-        return self.spriteManager.currentSprite()
+        return self.spriteManager.animate()
 
 
 class Goo(GameCollectible):
@@ -79,15 +84,14 @@ class Goo(GameCollectible):
         self._sprites = {
             'Goo_1': self._sheet.sprite(0, 1, 32, 16),
             'Goo_2': self._sheet.sprite(33, 1, 32, 16),
-            'Goo_3': self._sheet.sprite(99, 1, 32, 16),
-            'Goo_4': self._sheet.sprite(66, 1, 32, 16)
+            'Goo_3': self._sheet.sprite(66, 1, 32, 16),
+            'Goo_4': self._sheet.sprite(99, 1, 32, 16)
         }
 
         self.spriteManager = SpriteManager(self._sprites)
-        self.spriteManager.useSprites(['Goo_1', 'Goo_2', 'Goo_3', 'Goo_4', 'Goo_3', 'Goo_2'], 30)
+        self.spriteManager.useSprites(['Goo_1', 'Goo_2', 'Goo_3', 'Goo_4', 'Goo_3', 'Goo_2'], 8)
 
     def update(self):
-        self.spriteManager.animate()
         self.tick += 1
         if self.tick >= self.rand:
             self.kill()
