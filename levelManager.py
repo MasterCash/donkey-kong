4968647a64e8ffa0aa729cfa26cb24ce21cafc4c
Michael Rouse <mwrouse@outlook.com>
2018-11-27 19:52:04 -0600

    Correct positioning of immovables and one on the first platform.

--- levelManager.py
+++ levelManager.py
@@ -149,7 +149,7 @@ class LevelManager(GameLevelManager):
                 y = y - 1
                 lastX = x
 
-            self.immovables.add(InvisiblePlatform(x, y+1, self._invisiblePlatform))
+            self.immovables.add(InvisiblePlatform(0, y+1, self._invisiblePlatform))
 
             for i in range(0, len(platformsThisTime)):
                 if i > 0:
@@ -176,6 +176,8 @@ class LevelManager(GameLevelManager):
             self.platforms.add(Platform(x, y, self._platform))
             y = y - 1
             lastX = x
+
+        self.immovables.add(InvisiblePlatform(lastX, y+1, self._invisiblePlatform))
 
         drawLadder(lastX - (2*w), y) # Ladder from first platform
 
